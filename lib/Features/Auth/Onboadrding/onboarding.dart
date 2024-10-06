cfa4572f70cb70d9846ca127cb5e534968a74f39
Youssef-Khorshed <youssefmohamedeslayed2000@gmail.com>
2024-10-06 16:23:47 +0300

    add Theme (colors - styles) add helper (strings - spacing) add onboarding screen ios & android add sign up page edit main by adding (Doctor app) widget edit widget test file

--- lib/Features/Auth/Onboadrding/onboarding.dart
+++ lib/Features/Auth/Onboadrding/onboarding.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/Core/Helpers/spacing.dart';
+import 'package:flutter_complete_project/Core/Theme/Color/colors.dart';
 import 'package:flutter_complete_project/Features/Auth/Onboadrding/onboarding_Body.dart';
 import 'package:flutter_complete_project/Features/Auth/Onboadrding/onboarding_Footer.dart';
 import 'package:flutter_complete_project/Features/Auth/Onboadrding/onboarding_Header.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: SafeArea(
-            child: CustomScrollView(
-      slivers: [OnboardingHeader(), OnboardingBody(), OnboardingFooter()],
+            child: Padding(
+      padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+      child: CustomScrollView(
+        slivers: [OnboardingHeader(), OnboardingBody(), OnboardingFooter()],
+      ),
     )));
   }
 }
